24add1c6689e018a8b47c38ec71aefdaa634c4a2
fengyang <--->
2022-10-16 23:49:39 +0800

    配置初始化

---
+++ init.lua
@@ -0,0 +1,17 @@
+-- vim.cmd('set number')
+vim.o.number = true
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.smartindent = true
+vim.o.termguicolors = true
+vim.o.cursorline = true
+
+vim.o.mapleader = '<Alt>'
+
+
+
+vim.call('plug#begin', '~/.config/nvim/plugged')
+
+
+vim.call('plug#end')
+
