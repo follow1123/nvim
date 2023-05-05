2b443c8fd372c95826fe5d60e44af6b53e5e2f8d
fengyang <--->
2023-05-05 10:56:30 +0800

    添加自动命令配置文件、添加符号自动配对插件

--- lua/config.lua
+++ lua/config.lua
@@ -30,10 +30,3 @@ vim.wo.signcolumn = "yes"
 -- vim.o.showcmd = false
 -- 支持鼠标
 vim.o.mouse = 'a'
--- 去除回车后注释下一行
-vim.api.nvim_create_autocmd({ "BufEnter" }, {
-    pattern = "*",
-    callback = function()
-        vim.opt.formatoptions = vim.opt.formatoptions - { "c", "r", "o" }
-    end,
-})
