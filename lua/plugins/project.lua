91b7770251f92a4dcb7b9c3a58ae5c3982ba69e4
yf <--->
2023-08-30 20:48:48 +0800

    整合telescope和project插件，添加文件管理telescope扩展，优化telescope部分picker

--- lua/plugins/project.lua
+++ lua/plugins/project.lua
@@ -1,4 +1,3 @@
-
 -- 根据session list 获取对应session的root
 local function get_session_root(path)
   local session_dir = require("persistence.config").options.dir
@@ -8,29 +7,99 @@ local function get_session_root(path)
   return path:sub(1, path:len() - 4)
 end
 
--- 选择一个最近的项目打开
-local function select_projects()
-  local recent_projects = require("project_nvim").get_recent_projects()
-  vim.ui.select(recent_projects, {
-    prompt = "select project",
-  },
-  -- 切换当前目录到选择的项目
-  function (choice)
-    if choice then
-      local persistence = require("persistence")
-      local session_list = persistence.list()
-      for _, value in pairs(session_list) do
-        if choice:match(get_session_root(value)) then
-          vim.cmd("%bdelete!")
-          vim.cmd("silent! source " .. vim.fn.fnameescape(value))
-          break
+
+-- local function create_finder()
+--   local results = history.get_recent_projects()
+--
+--   -- Reverse results
+--   for i = 1, math.floor(#results / 2) do
+--     results[i], results[#results - i + 1] = results[#results - i + 1], results[i]
+--   end
+--   local displayer = entry_display.create({
+--     separator = " ",
+--     items = {
+--       {
+--         width = 30,
+--       },
+--       {
+--         remaining = true,
+--       },
+--     },
+--   })
+--
+--   local function make_display(entry)
+--     return displayer({ entry.name, { entry.value, "Comment" } })
+--   end
+--
+--   return finders.new_table({
+--     results = results,
+--     entry_maker = function(entry)
+--       local name = vim.fn.fnamemodify(entry, ":t")
+--       return {
+--         display = make_display,
+--         name = name,
+--         value = entry,
+--         ordinal = name .. " " .. entry,
+--       }
+--     end,
+--   })
+-- end
+
+-- local function delete_project(prompt_bufnr)
+--   local state = require("telescope.actions.state")
+--   local history = require("project_nvim.utils.history")
+--   local selectedEntry = state.get_selected_entry(prompt_bufnr)
+--   local actions = require("telescope.actions")
+--   if selectedEntry == nil then
+--     actions.close(prompt_bufnr)
+--     return
+--   end
+--   local choice = vim.fn.confirm("Delete '" .. selectedEntry.value .. "' from project list?", "&Yes\n&No", 2)
+--
+--   if choice == 1 then
+--     history.delete_project(selectedEntry)
+--
+--     local finder = create_finder()
+--     state.get_current_picker(prompt_bufnr):refresh(finder, {
+--       reset_prompt = true,
+--     })
+--   end
+-- end
+-- 由于project.nvim插件实现的telescope扩展不规范，需要使用这种方式修改自定义按键
+_G.select_projects = function()
+  local state = require("telescope.actions.state")
+  local actions = require("telescope.actions")
+  require('telescope').extensions.projects.projects{
+    attach_mappings = function(prompt_bufnr, map) -- telescope配置按键映射方法
+      -- map("i", "<M-d>", function ()
+      --   
+      -- end)
+      actions.select_default:replace(function () -- telescope修改选择默认方法
+        -- 获取选项的值
+        local selected_entry = state.get_selected_entry(prompt_bufnr)
+        if selected_entry == nil then
+          actions.close(prompt_bufnr)
+          return
         end
-      end
-      -- 开启保存session功能
-      persistence.start()
-      vim.api.nvim_set_current_dir(choice)
-    end
-  end)
+        local project_path = selected_entry.value
+        actions.close(prompt_bufnr)
+
+        -- 查找对应的session并打开
+        local persistence = require("persistence")
+        local session_list = persistence.list()
+        for _, value in pairs(session_list) do
+          if project_path:match(get_session_root(value)) then
+            vim.cmd("%bdelete!")
+            vim.cmd("silent! source " .. vim.fn.fnameescape(value))
+            break
+          end
+        end
+        persistence.start() -- 开启保存session功能
+        vim.api.nvim_set_current_dir(project_path) -- 切换当前工作目录
+      end)
+      return true
+    end,
+  }
 end
 
 return {
@@ -52,9 +121,6 @@ return {
         scope_chdir = 'global',
         datapath = vim.fn.stdpath("data"),
       }
-      local opts = { noremap = false,  silent = true }
-      opts.desc = "open a recent project directory"
-      vim.keymap.set("n", "<leader>pf", select_projects, opts)
     end
   },
   { -- session
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,41 +1,115 @@
-local live_grep_key = "<C-A-f>"
-if _G.IS_GUI then
-	live_grep_key = "<C-f>"
-end
 -- 搜索插件
 return {
-	"nvim-telescope/telescope.nvim",
-	version = "0.1.x",
-	keys = {
-		{ "<A-f>",      ":Telescope find_files<CR>", desc = "find files" },
-		{ live_grep_key, ":Telescope live_grep<CR>", desc = "live grep"	},
-		{ "<leader>fb", ":Telescope buffers<CR>",    desc = "buffers" },
-		{ "<leader>fh", ":Telescope help_tags<CR>",  desc = "help tags" },
-	},
-	-- 搜索依赖插件
-	dependencies = { "nvim-lua/plenary.nvim" },
-	config = function()
-		-- local builtin = require("telescope.builtin")
-		local actions = require("telescope.actions")
-		require("telescope").setup {
-			defaults = {
-				mappings = {
-					i = {
-						["<C-j>"] = actions.move_selection_next,
-						["<C-k>"] = actions.move_selection_previous,
-					}
-				}
-			},
-			pickers = {
-				find_files = {
-					theme = "dropdown",
-					previewer = false,
-					find_command = { "fd" },
-				},
-				live_grep = {
-					theme = "ivy",
-				}
-			}
-		}
-	end
+  {
+    "nvim-telescope/telescope.nvim",
+    version = "0.1.x",
+    keys = {
+      { "<A-f>",      ":Telescope find_files<CR>",                         desc = "find files" },
+      { "<C-f>",      ":Telescope current_buffer_fuzzy_find<CR>",          desc = "find in current buffer" },
+      { "<leader>ff", ":Telescope live_grep<CR>",                          desc = "live grep" },
+      { "<leader>fb", ":Telescope buffers<CR>",                            desc = "buffers" },
+      { "<leader>fh", ":Telescope help_tags<CR>",                          desc = "help tags" },
+
+      -- vim内置帮助相关
+      { "<leader>/c", ":Telescope commands<CR>",                           desc = "list commands" },
+      { "<leader>/h", ":Telescope highlights<CR>",                         desc = "list highlights" },
+      { "<leader>/r", ":Telescope registers<CR>",                          desc = "list registers" },
+      { "<leader>/f", ":Telescope filetypes<CR>",                          desc = "list filetypes" },
+      { "<leader>/t", ":Telescope colorscheme<CR>",                        desc = "list colorscheme" },
+      { "<leader>/k", ":Telescope keymaps<CR>",                            desc = "list keymaps" },
+      { "<leader>/a", ":Telescope autocommands<CR>",                       desc = "list autocommands" },
+      { "<leader>pf", "<cmd>lua _G.select_projects()<cr>", desc = "list projects" },
+    },
+    -- 搜索依赖插件
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      -- local themes = require("telescope.themes")
+      local actions = require("telescope.actions")
+      local state = require("telescope.actions.state")
+      -- 搜索相关picker 默认搜索后居中光标
+      local function select_default_and_center(prompt_bufnr)
+        actions.select_default(prompt_bufnr)
+        vim.cmd("normal! zz")
+      end
+
+
+      require("telescope").setup {
+        defaults = {
+          prompt_prefix = "  ",
+          selection_caret = "❯ ",
+          mappings = {
+            i = {
+              ["<C-j>"] = actions.move_selection_next,                           -- 下一个
+              ["<C-k>"] = actions.move_selection_previous,                       -- 上一个
+
+              ["<C-n>"] = actions.cycle_history_next,                            -- 历史记录上一个
+              ["<C-p>"] = actions.cycle_history_prev,                            -- 历史记录下一个
+
+              ["<C-c>"] = actions.close,                                         -- 关闭
+
+              ["<C-x>"] = actions.select_horizontal,                             -- 水平分屏打开
+              ["<C-v>"] = actions.select_vertical,                               -- 垂直分屏打开
+
+              ["<C-u>"] = actions.preview_scrolling_up,                          -- 预览窗口向上滚动
+              ["<C-d>"] = actions.preview_scrolling_down,                        -- 预览窗口向下滚动
+
+              ["<C-w>"] = actions.send_selected_to_qflist + actions.open_qflist, -- 使用tab或shift tab选中后按ctrl w打开一个窗口单独显示选中的数据
+            }
+          }
+        },
+        pickers = {
+          find_files = { -- 查找文件设置
+            previewer = false,
+            find_command = { "fd" },
+          },
+          current_buffer_fuzzy_find = { -- 搜索当前buffer设置
+            theme = "ivy",
+            mappings = {
+              i = {
+                ["<CR>"] = select_default_and_center
+              }
+            }
+          },
+          help_tags = { --搜索帮助设置
+            mappings = {
+              i = {
+                ["<CR>"] = actions.select_vertical, -- 帮助页面默认垂直分屏显示
+              }
+            }
+          },
+          buffers = {
+            mappings = {
+              i = {
+                ["<M-d>"] = function (prompt_bufnr)
+                  local selected_entry = state.get_selected_entry(prompt_bufnr)
+                  if selected_entry == nil then
+                    actions.close(prompt_bufnr)
+                    return
+                  end
+                  local bufnr = selected_entry.bufnr
+                  actions.close(prompt_bufnr)
+                  vim.api.nvim_buf_delete(bufnr, { force = true })
+                  require("telescope.builtin").buffers() -- 关闭后重新打开
+                end
+              }
+            }
+          }
+        },
+        extensions = {
+          file_browser = {
+            theme = "ivy",
+            hijack_netrw = true,
+          },
+        }
+      }
+
+      require("telescope").load_extension("projects")
+      require("telescope").load_extension("file_browser")
+    end
+  },
+  {
+    "nvim-telescope/telescope-file-browser.nvim",
+    lazy = true,
+    dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" }
+  },
 }
