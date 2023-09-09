8c663b8d45908ab365f3177be6d47a09817a0788
yf <--->
2023-09-09 20:13:08 +0800

    添加删除key的keymap工具

--- lua/utils/keymap.lua
+++ lua/utils/keymap.lua
@@ -13,6 +13,7 @@ end
 
 keymap.buf_map = function(mode, lhs, rhs, opts, bufnr)
   opts = vim.fn.empty(opts) == 0 and opts or nil
+  bufnr = bufnr and tonumber(bufnr) or 0
   local def_opts = { buffer = bufnr }
   if type(opts) == "string" then
     def_opts.desc = opts
@@ -20,6 +21,18 @@ keymap.buf_map = function(mode, lhs, rhs, opts, bufnr)
     def_opts = vim.tbl_extend("force", def_opts, opts)
   end
   vim.keymap.set(mode, lhs, rhs, def_opts)
+end
+
+keymap.del_map = function(mode, lhs, bufnr)
+  lhs = vim.fn.empty(lhs) == 0 and lhs or {}
+  local opts = bufnr and {buffer = bufnr} or nil
+  if type(lhs) == "string" then
+    vim.keymap.del(mode, lhs, opts)
+  elseif type(lhs) == "table" then
+    for _, l in ipairs(lhs) do
+      vim.keymap.del(mode, l, opts)
+    end
+  end
 end
 
 keymap.lazy_map = function(mode, lhs, rhs, opts)
