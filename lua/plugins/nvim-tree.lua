f37479912461b8b3b11862670add6ae2d464ba21
yf <--->
2023-09-09 20:15:04 +0800

    修改nvimtree的快捷键尽量和lf一致，设置文件git的颜色

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,3 +1,7 @@
+local keymap_uitl = require("utils.keymap")
+local lazy_map = keymap_uitl.lazy_map
+local buf_map = keymap_uitl.buf_map
+local del_map = keymap_uitl.del_map
 -- 判断当前启动的是文件还是目录
 -- 文件树插件
 return {
@@ -5,11 +9,27 @@ return {
   lazy = vim.fn.isdirectory(vim.fn.expand("%:p")) == 0,
   -- nvim打开目录则直接加载插件，否则使用key懒加载
   keys = {
-    { "<M-1>", ":NvimTreeFindFileToggle<CR>", desc = "find file in nvim tree", silent = true },
+    lazy_map("n", "<M-1>", "<cmd>NvimTreeFindFileToggle<cr>", "nvim-tree: Find file in nvim tree"),
   },
 	config = function()
+
+    -- 代码诊断
+    -- vim.api.nvim_set_hl(0, "DiagnosticsError", {sp = "Red", undercurl = true})
+    -- vim.api.nvim_set_hl(0, "DiagnosticsWarning", {sp = "Yellow", undercurl = true})
+    -- vim.api.nvim_set_hl(0, "DiagnosticsInformation", {sp = "#4fc1ff", undercurl = true})
+    -- vim.api.nvim_set_hl(0, "DiagnosticsHint", {sp = "Blue", undercurl = true})
+    -- git
+    vim.api.nvim_set_hl(0, "NvimTreeFileDirty", {fg = "#1c7ca1"})
+    vim.api.nvim_set_hl(0, "NvimTreeFileStaged", {fg = "#536232"})
+    vim.api.nvim_set_hl(0, "NvimTreeFileMerge", {fg = "#e0ebff"})
+    -- vim.api.nvim_set_hl(0, "NvimTreeFileRenamed", {fg = ""})
+    vim.api.nvim_set_hl(0, "NvimTreeFileNew", {fg = "#771b1b"})
+    vim.api.nvim_set_hl(0, "NvimTreeFileDeleted", {fg = "Red"})
+    vim.api.nvim_set_hl(0, "NvimTreeFileIgnored", {fg = "#3e3e3e", strikethrough = true})
+
+    vim.api.nvim_set_hl(0, "NvimTreeNormal", { bg = "#1e1e1e" })
 		-- 插件配置
-		-- local api = require("nvim-tree.api")
+		local api = require("nvim-tree.api")
 		require("nvim-tree").setup {
       disable_netrw = true, -- 禁用默认netrw插件，已在plugin_init文件内禁用
       hijack_netrw = true, -- 使用nvim-tree代替netrw
@@ -21,18 +41,66 @@ return {
       },
       -- 不显示 git 状态图标
       git = {
-        enable = false
+        enable = true,
+        show_on_dirs = true,
+      },
+      diagnostics = {
+        enable = true
+      },
+      actions = {
+        file_popup = {
+          open_win_config = {
+            border = "single" -- 修改文件信息的边框类型
+          }
+        }
       },
       -- 不显示以.开头的隐藏文件
       filters = {
         dotfiles = true,
       },
+      on_attach = function (bufnr)
+        -- 默认按键
+        api.config.mappings.default_on_attach(bufnr)
+
+        -- 自定义按键 尽量和lf配置一致
+        buf_map("n", "<leader>h", api.tree.toggle_help, "nvim-tree: Help", bufnr)
+
+        -- 删除默认的快捷键
+        del_map("n", {"y", "Y", "x", "R", "r", "c", "C"}, bufnr)
+
+        -- 文件操作
+        buf_map("n", "dd", api.fs.cut, "nvim-tree: Cut", bufnr)
+        buf_map("n", "x", api.fs.trash, "nvim-tree: Trash", bufnr)
+        buf_map("n", "X", api.fs.remove, "nvim-tree: Remove", bufnr)
+        buf_map("n", "<F2>", api.fs.rename_basename, "nvim-tree: Rname basename", bufnr)
+        buf_map("n", "R", api.fs.rename, "nvim-tree: Rname", bufnr)
+        buf_map("n", "yy", api.fs.copy.node, "nvim-tree: Copy", bufnr)
+        buf_map("n", "yn", api.fs.copy.filename, "nvim-tree: Copy name", bufnr)
+        buf_map("n", "yp", api.fs.copy.relative_path, "nvim-tree: Copy relative path", bufnr)
+        buf_map("n", "yP", api.fs.copy.absolute_path, "nvim-tree: Copy absolute path", bufnr)
+
+        -- 查找
+        buf_map("n", "<C-f>", api.live_filter.start, "nvim-tree: Filter file", bufnr)
+        buf_map("n", "<Esc>", api.live_filter.clear, "nvim-tree: Filter clean", bufnr)
+        -- 其他
+        buf_map("n", "r", api.tree.reload, "nvim-tree: Reload", bufnr)
+        buf_map("n", "C", api.tree.collapse_all, "nvim-tree: Collapse all", bufnr)
+
+
+      end,
       renderer = {
+        highlight_git = true,
+        icons = {
+          show = {
+            git = false,
+          }
+        },
         indent_markers = {
           enable = true, -- 开启目录树的缩进线
         },
       }
     }
-    vim.api.nvim_set_hl(0, "NvimTreeNormal", { bg = "#1e1e1e" })
+
+
   end
 }
