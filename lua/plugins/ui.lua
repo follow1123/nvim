88c1cbea56fff677e8aa929669567f47730c0eba
yf <--->
2023-09-09 20:12:48 +0800

    设置空格字符

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -111,6 +111,7 @@ return {
       vim.opt.list = true
       -- vim.opt.listchars:append "space:⋅"
       vim.opt.listchars:append "eol:↴"
+      vim.opt.listchars:append "trail: "
       vim.api.nvim_set_hl(0, "NonText", { fg = "#3e3e3e"})
       require("indent_blankline").setup {
         space_char_blankline = " ",
