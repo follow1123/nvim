323473b9f2cf3e9d95ebf4c748232268330ba26b
yf <--->
2023-12-10 18:48:10 +0800

    去除打开gui nvim时自动打开上一个项目，修改为快捷键打开

--- lua/plugins/project.lua
+++ lua/plugins/project.lua
@@ -7,18 +7,15 @@ return {
   lazy = not _G.IS_GUI,
   keys = {
     lazy_map("n", "<leader>pf", "<cmd>lua require('telescope').extensions.projects.recent_projects()<cr>", "project: List recent projects"),
-    lazy_map("n", "<leader>pa", "<cmd>lua require('project_session').add()<cr>", "project: Add project"),
+    lazy_map("n", "<leader>pa", "<cmd>lua require('project_session').add()<cr>", "project: Add this project"),
     lazy_map("n", "<leader>po", "<cmd>lua require('project_session').open()<cr>", "project: Open project"),
     lazy_map("n", "<leader>ps", "<cmd>lua require('project_session').save()<cr>", "project: Save project"),
+    lazy_map("n", "<leader>pr", "<cmd>lua require('project_session').load_last()<cr>", "project: Load last project"),
   },
   config = function()
     local project_session = require("project_session")
     project_session.setup({
       options = { "buffers", "curdir", "tabpages", "winsize", "folds" },
     })
-    if _G.IS_GUI then
-      project_session.load_last()
-      pcall(vim.api.nvim_command, "TSEnable highlight")
-    end
   end
 }
