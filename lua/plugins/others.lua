69141a37b180f30f26f099c67e21ae7ce1fa1211
yf <--->
2024-05-12 23:37:28 +0800

    feat: 添加harpoon插件

--- lua/plugins/others.lua
+++ lua/plugins/others.lua
@@ -65,6 +65,24 @@ return {
     config = function()
       require("Comment").setup()
     end
+  },
+  {
+    "ThePrimeagen/harpoon",
+    keys = {
+      lazy_map("n", "<leader>fa", "<cmd>lua require('harpoon'):list():add()<cr>", "harpoon: Add file to harpoon list"),
+      lazy_map("n", "<leader>fl", "<cmd>lua require('harpoon').ui:toggle_quick_menu(require('harpoon'):list())<cr>", "harpoon: Toggle harpoon list"),
+      lazy_map("n", "<S-h>", "<cmd>lua require('harpoon'):list():prev()<cr>", "harpoon: Goto previous file in harpoon list"),
+      lazy_map("n", "<S-l>", "<cmd>lua require('harpoon'):list():next()<cr>", "harpoon: Goto next file in harpoon list"),
+      lazy_map("n", "<M-j>", "<cmd>lua require('harpoon'):list():select(1)<cr>", "harpoon: Goto next first file in harpoon list"),
+      lazy_map("n", "<M-k>", "<cmd>lua require('harpoon'):list():select(2)<cr>", "harpoon: Goto next second in harpoon list"),
+      lazy_map("n", "<M-l>", "<cmd>lua require('harpoon'):list():select(3)<cr>", "harpoon: Goto next third in harpoon list"),
+      lazy_map("n", "<M-;>", "<cmd>lua require('harpoon'):list():select(4)<cr>", "harpoon: Goto next fourth in harpoon list"),
+    },
+    branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("harpoon").setup()
+    end
   }
 }
 
