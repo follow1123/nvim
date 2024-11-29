78e3085e834789114ae838577b34fc12235ff6ce
yf <--->
2024-11-29 20:16:21 +0800

    reconfig: 创建分屏终端类

--- lua/extensions/terminal/init.lua
+++ lua/extensions/terminal/init.lua
@@ -7,6 +7,6 @@ M.scratch_term = require("extensions.terminal.scratch_term"):new()
 
 M.lazygit_term = require("extensions.terminal.lazygit_term"):new()
 
-M.split_term = require("extensions.terminal.split_term"):new()
+M.split_term = require("extensions.terminal.split_terminal"):new("pwsh")
 
 return M
