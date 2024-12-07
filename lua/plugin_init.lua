f1d08ce758433de846ad1071dd873ce5e6bb949f
yf <--->
2024-12-07 15:09:35 +0800

    feat: 更新lazy.nvim插件管理器

--- lua/plugin_init.lua
+++ lua/plugin_init.lua
@@ -1,17 +1,26 @@
 local colors = require("utils.colors")
+-- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git", "clone", "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git", "--branch=v10.20.3", -- latest stable release
-    lazypath
-  })
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=v11.15.0", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
 end
 vim.opt.rtp:prepend(lazypath)
 
+-- Setup lazy.nvim
 require("lazy").setup({
   spec = {
-    import = "plugins"
+    -- import your plugins
+    { import = "plugins" },
   },
   ui = {
     border = "single"
@@ -19,6 +28,8 @@ require("lazy").setup({
   change_detection = {
     notify = false,
   },
+  rocks = { enabled = false }
 })
+
 vim.api.nvim_set_hl(0, "LazyProp", { bg = colors.gray_04 })
 require("utils.keymap").nmap("<leader>1", "<cmd>Lazy<cr>", "Open Lazy Manager panel")
