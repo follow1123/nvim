86475f3b149e1ffb48d94a13f6bccb572610f398
yf <--->
2024-12-09 19:54:52 +0800

    refactor: 简化lsp状态实现

--- lua/plugins/lsp/lsp-status.lua
+++ lua/plugins/lsp/lsp-status.lua
@@ -1,7 +1,6 @@
 ---@class LspStatus
----@field clients table<string, string>
----@field timer uv.uv_timer_t
 ---@field default_statusline string
+---@field current_frame_idx integer
 local LspStatus = {}
 
 local group = vim.api.nvim_create_augroup("LSP_STATUS", { clear = true })
@@ -14,9 +13,7 @@ function LspStatus:init()
     callback = function()
       -- 参考 vim.lsp.status() 方法源码实现
       local percentage = nil
-      local percentage_map = {} --- @type table<string, integer>
       for _, client in ipairs(vim.lsp.get_clients()) do
-        percentage_map[client.name] = -1
         --- @diagnostic disable-next-line:no-unknown
         for progress in client.progress do
           --- @cast progress {token: lsp.ProgressToken, value: lsp.LSPAny}
@@ -24,80 +21,40 @@ function LspStatus:init()
           if type(value) == 'table' and value.kind then
             if value.percentage then
               percentage = math.max(percentage or 0, value.percentage)
-              percentage_map[client.name] = percentage
             end
           end
           -- else: Doesn't look like work done progress and can be in any format
           -- Just ignore it as there is no sensible way to display it
         end
       end
-      for client_name, per in pairs(percentage_map) do
-        if per == -1 then
-          self:stop(client_name)
-        else
-          self:start(client_name)
-        end
+      if percentage then
+        self:render_next_frame()
+      else
+        self:reset_statusline()
       end
     end
   })
-  vim.api.nvim_create_autocmd("VimLeave", {
-    group = group,
-    callback = function()
-      if self.timer then self.timer:close() end
-    end,
-  })
-  self.clients = {}
   self.default_statusline = vim.o.statusline
+  self.current_frame_idx = 1
 end
 
----@param frame_idx integer
-function LspStatus:update_statusline(frame_idx)
-  vim.schedule(function()
-    local status_table = { "%y%m ", self.spinner_frames[frame_idx], " LSP Loading...%=%<%F %r%=%-14.(%l,%c%V%) %P" }
-    vim.o.statusline = table.concat(status_table)
-  end)
+function LspStatus:render_next_frame()
+  local frame = self.spinner_frames[self.current_frame_idx]
+  self:update_statusline(frame)
+  self.current_frame_idx = self.current_frame_idx + 1
+  if self.current_frame_idx > #self.spinner_frames then
+    self.current_frame_idx = 1
+  end
+end
+
+---@param frame string
+function LspStatus:update_statusline(frame)
+  local status_table = { "%y%m ", frame, " LSP Loading …%=%<%F %r%=%-14.(%l,%c%V%) %P" }
+  vim.o.statusline = table.concat(status_table)
 end
 
 function LspStatus:reset_statusline()
   vim.o.statusline = self.default_statusline
-end
-
-function LspStatus:has_loading_clients()
-  return not vim.tbl_isempty(self.clients)
-end
-
-function LspStatus:loading()
-  if self.timer == nil then
-    local timer = vim.uv.new_timer()
-    assert(timer, "create timer error")
-    self.timer = timer
-  end
-  local frames_num = #self.spinner_frames
-  local frame_idx = 0
-  self.timer:start(0, 100, function()
-    frame_idx = frame_idx % frames_num
-    self:update_statusline(frame_idx + 1)
-    frame_idx = frame_idx + 1
-  end)
-end
-
-function LspStatus:done()
-  if self.timer == nil then return end
-  self.timer:stop()
-  self:reset_statusline()
-end
-
----@param client_name string
-function LspStatus:start(client_name)
-  if self.clients[client_name] then return end
-  self.clients[client_name] = client_name
-  self:loading()
-end
-
----@param client_name string
-function LspStatus:stop(client_name)
-  self.clients[client_name] = nil
-  self:done()
 end
 
 return LspStatus
