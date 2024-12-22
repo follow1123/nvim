2a08714001e7afe3bf615863f51256f7655e8771
yf <--->
2024-12-22 20:40:41 +0800

    feat: 添加特殊搜索内容命令，支持glob，删除不常用按键配置

---
+++ lua/plugins/telescope/multigrep.lua
@@ -0,0 +1,45 @@
+local pickers = require("telescope.pickers")
+local finders = require("telescope.finders")
+local make_entry = require("telescope.make_entry")
+local conf = require("telescope.config").values
+
+local M = {}
+
+function M.multigrep()
+  local opts = { cwd = vim.uv.cwd() }
+  local finder = finders.new_async_job({
+    command_generator = function(prompt)
+      if prompt == nil or prompt == "" then return nil end
+      local args = vim.list_extend({}, conf.vimgrep_arguments)
+      table.insert(args, "--path-separator")
+      table.insert(args, "/")
+      local pieces = vim.split(prompt, "  ")
+      local content
+      if #pieces > 1 then
+        table.insert(args, "-g")
+        table.insert(args, pieces[#pieces])
+        table.remove(pieces, #pieces)
+        content = table.concat(pieces, "  ")
+      else
+        content = pieces[1]
+      end
+      table.insert(args, "-e")
+      table.insert(args, content)
+
+      -- rg --color=never --no-heading --with-filename --line-number --column --smart-case --path-separator / -e <content> [-g <globs>]
+      return args
+    end,
+    cwd = opts.cwd,
+    entry_maker = make_entry.gen_from_vimgrep(opts)
+  })
+  pickers.new(opts, {
+    debounce = 100,
+    prompt_title = "Multi Grep",
+    finder = finder,
+    previewer = conf.grep_previewer(opts),
+    sorter = require("telescope.sorters").empty()
+  }):find()
+end
+
+
+return M
