0ceaf926aa7b5d18dd950fc1fb59ab1c5bf57c29
yf <--->
2025-04-26 23:45:34 +0800

    feat: 添加shell文件内直接执行shell命令的快捷键

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -80,3 +80,37 @@ vim.api.nvim_create_autocmd("FileType", {
     vmap("<space>x", ":lua<cr>", "lua: execute selected code", args.buf)
   end
 })
+
+vim.api.nvim_create_autocmd("FileType", {
+  group = base_group,
+  pattern = "sh",
+  desc = "set some options in shell file",
+  callback = function(args)
+    local nmap = require("utils.keymap").nmap
+    local vmap = require("utils.keymap").vmap
+
+    nmap("<space>x", ":execute '!' . getline('.')<cr>", "shell: execute code", args.buf)
+    vmap("<space>x", function()
+      vim.api.nvim_input("<esc>")
+      vim.schedule(function ()
+        -- 获取选区的起止行（注意 Lua 下标从 1 开始，get_lines 是从 0 开始）
+        local start_pos = vim.api.nvim_buf_get_mark(0, "<")
+        local end_pos = vim.api.nvim_buf_get_mark(0, ">")
+        local start_line = start_pos[1] - 1
+        local end_line = end_pos[1]
+
+        -- 获取选中的行内容
+        local lines = vim.api.nvim_buf_get_lines(0, start_line, end_line, false)
+
+        -- 拼接为一整个 bash 命令
+        local cmd = table.concat(lines, "\n")
+
+        -- 执行命令
+        local result = vim.fn.system({ "bash", "-c", cmd })
+
+        -- 显示输出结果
+        vim.notify(result, vim.log.levels.INFO)
+      end)
+    end, "shell: execute selected code", args.buf)
+  end
+})
