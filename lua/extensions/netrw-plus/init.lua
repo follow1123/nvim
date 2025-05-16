b01c9253432793cfa872260f49ab07f30138e5bd
yf <--->
2025-05-16 16:51:39 +0800

    feat: 去除netrw窗口分屏时自动定位焦点窗口的文件功能

--- lua/extensions/netrw-plus/init.lua
+++ lua/extensions/netrw-plus/init.lua
@@ -15,11 +15,11 @@ vim.api.nvim_create_autocmd("FileType", {
     nmap("<C-h>", "<C-w>h", "netrw: Go to left window", e.buf)
     nmap("<C-l>", "<C-w>l", "netrw: Go to right window", e.buf)
     -- 复制光标下文件的绝对路径
-    nmap("yP", function ()
+    nmap("yP", function()
       vim.fn.setreg("+", netrw:get_file_path())
     end, "netrw: Copy absolute path", e.buf)
     -- 复制光标下文件的相对路径
-    nmap("yp", function ()
+    nmap("yp", function()
       vim.fn.setreg("+", vim.fn.fnamemodify(netrw:get_file_path(), ":."))
     end, "netrw: Copy relative path", e.buf)
     -- 搜索文件或目录
@@ -49,35 +49,5 @@ nmap("<leader>fL", function()
   local file_full_path = vim.api.nvim_buf_get_name(buf)
   netrw:locate_file(file_full_path, netrw_buf)
 end, "netrw: Locate current file in netrw buffer")
-
--- 切换窗口后netrw窗口自动定位当前文件
-vim.api.nvim_create_autocmd({ "WinEnter", "BufWinEnter" }, {
-  pattern = vim.fs.normalize(vim.fn.getcwd()) .. "/*",
-  group = netrw_group,
-  desc = "netrw: Locate file in netrw buffer when other window enter",
-  callback = function(e)
-    local netrw_buf = netrw:get_visible_netrw()
-    if netrw_buf == nil then return end
-    local buf = e.buf
-
-    -- 进入 netrw buffer 时定位之前的buffer
-    if buf == netrw_buf then
-      -- 判断上次访问的 buffer 是不是就是当前 buffer
-      local prev_win_buf = vim.fn.winbufnr(vim.fn.winnr("#"))
-      if buf == prev_win_buf then return end
-      -- 获取上次访问 buffer 的文件路径信息
-      local file_full_path = vim.api.nvim_buf_get_name(prev_win_buf)
-      -- 判断是否是正常文件 buffer
-      if not util.is_file(file_full_path) then return end
-      -- 判断是否是当前工作目录内的文件
-      if not netrw.in_working_dir(file_full_path) then return end
-      vim.schedule_wrap(netrw.locate_file)(netrw, file_full_path)
-      return
-    end
-
-    local file_full_path = vim.api.nvim_buf_get_name(buf)
-    netrw:locate_file(file_full_path, netrw_buf)
-  end
-})
 
 return netrw
